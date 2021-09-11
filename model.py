ae1f098c8bc09d3663a898d91531928da7568afd
EldadKronfeld <eldadkro@gmail.com>
2021-09-11 09:31:48 +0300

    finished model

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 #%%
+from math import log
 import numpy as np
 import matplotlib.pyplot as pp
 import random as rnd
 import scipy.optimize as op
-
-
+from itertools import product 
+from scipy.optimize import minimize,LinearConstraint
 class simulation1():
     
     def __init__(self,ticks,start,f,prob) -> None:
@@ -53,7 +54,7 @@ class probD():
         return op.brentq(func,0.000001,0.99999)
 
     
-class simulationMulti():
+class simMulti1W():
     def __init__(self,ticks,start,f,b,prob) -> None:
        self.ticks = ticks
        self.prob = prob
@@ -64,7 +65,7 @@ class simulationMulti():
        self.res[0] = start
     
     def _genAll(self):
-        vals = np.empty((len(f)))
+        vals = np.empty((len(self.prob)))
         for i,prob in enumerate(self.prob):
             vals[i] = prob.gen()
         return vals
@@ -79,6 +80,45 @@ class simulationMulti():
                 # self.res[index]*=tmp[j]
             index += 1
         return self.res
+
+
+def combinations(probabilities):
+    probs = list()
+    for p in probabilities:
+        probs.append(range(0,len(p.vec[0])))
+    products = product(*probs)
+    return products
+
+def target_factory(probabilities):
+    iter_comb = combinations(probabilities)
+    # look at probablity calc optimization
+    def target(f:np.array):
+        sum = 0
+        for comb in iter_comb:
+            s = 1
+            x = np.empty((len(probabilities)))
+            index = 0
+            for p,i in zip(probabilities,comb):
+                v = p.vec
+                s *= v[0,i]
+                x[index] = v[1,i]
+                index += 1
+            s *= np.log(f[0] + x@f[1:])
+            sum += s
+        return -sum
+    return target
+
+
+def optimize_f(probabilities):
+    target = target_factory(probabilities)
+    const = {'type': 'eq', 'fun': lambda x:  sum(x) - 1}
+    bounds= ((0,None) for x in range(len(probabilities)))
+    bounds = list(bounds)
+    bounds.insert(0,(0.00001,None))
+    bounds = tuple(bounds)
+    return minimize(target,np.zeros(len(probabilities)+1),method='SLSQP',constraints=const,bounds=bounds)
+
+
 
 def main1():
     vec = np.array([[0.154,0.50,0.32,0.026],[1.5,0.651,-1,-1.93]])
@@ -99,18 +139,27 @@ def main1():
     pp.legend(["random f","optimized f"])
     pp.grid(True)
     pp.show()
-
-f = np.array([[0.7,0.3],[1.5,0.4]])
-probs = [probD(f) for i in range(3)]
-f = np.array([0.2,0.2,0.2])
-s = simulationMulti(100,1,f,0.4,probs)
-s.run()
-res = s.res
-x = np.linspace(0,100,101)
-pp.plot(x,res,'-*')
-pp.title("multi independent options, with multiple returns")
-pp.xlabel("tick")
-pp.ylabel("value")
-pp.legend(["random f"])
-pp.grid(True)
-pp.show()
+# main1()
+def main2():
+    f = np.array([[0.7,0.3],[1.5,0.4]])
+    probs = [probD(f) for i in range(3)]
+    
+    f_max = optimize_f(probs)
+    f_max = f_max['x']
+    f = np.array([0.2,0.2,0.2])
+    s = simMulti1W(100,1,f,0.4,probs)
+    s.run()
+    res = s.res
+    s_max = simMulti1W(100,1,f_max[1:],f_max[0],probs)
+    s_max.run()
+    res_max = s_max.res
+    x = np.linspace(0,100,101)
+    pp.plot(x,res,'-*')
+    pp.plot(x,res_max,'-*')
+    pp.title("multi independent options, with multiple returns")
+    pp.xlabel("tick")
+    pp.ylabel("value")
+    pp.legend(["random f"])
+    pp.grid(True)
+    pp.show()
+main2()
